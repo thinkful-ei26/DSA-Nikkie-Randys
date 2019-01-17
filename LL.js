e62253ae0fc3dae425615e831e15b9e95d693440
Nikkie Mashian <nikkiemashian@gmail.com>
2019-01-17 11:18:50 -0800

    insertAt

--- LL.js
+++ LL.js
@@ -38,7 +38,7 @@ class LinkedList {
     }
 
     if(key===this.head.data){
-      this.insertFirst(item)
+      this.insertFirst(item);
       return;
     }
 
@@ -47,30 +47,30 @@ class LinkedList {
     let prevNode = this.head;
 
     while(currentNode!==null){
-      console.log('in while loop')
+      console.log('in while loop');
       if(currentNode.data===key){
-        console.log('found corrent item')
-        newNode.next = currentNode
-        prevNode.next = newNode
+        console.log('found corrent item');
+        newNode.next = currentNode;
+        prevNode.next = newNode;
         return;
       } else {
         prevNode = currentNode;
-        currentNode = currentNode.next
+        currentNode = currentNode.next;
       }
     }
   }
 
   insertAfter(item, key){
     const newNode = new _Node(item, null);
-    let currentNode = this.head
+    let currentNode = this.head;
 
     if(this.head===null){
       this.insertFirst(item);
     }
 
     if(key===this.head.data){
-      newNode.next = currentNode.next
-      currentNode.next = newNode
+      newNode.next = currentNode.next;
+      currentNode.next = newNode;
       return;
     }
 
@@ -78,25 +78,46 @@ class LinkedList {
 
     while(currentNode!==null){
       if(currentNode.data===key){
-        newNode.next = currentNode.next
-        currentNode.next = newNode
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
         return;
       } else {
-        currentNode = currentNode.next
+        currentNode = currentNode.next;
       }
     }
   }
 
   insertAt(item, position){
-    const newNode = new _Node(item, null);
+    let newNode = new _Node(item, null);
     let currentNode = this.head;
     let prevNode = this.head;
+    let count = 1;
 
+    //if empty list
     if(this.head===null){
       this.insertFirst(item);
+      return;
     }
 
+    if(position===1){
+      console.log('in here');
+      this.insertFirst(item);
+      return;
+    }
 
+    while(currentNode!==null){
+      if(count===position){
+        newNode.next = currentNode;
+        prevNode.next = newNode;
+        return;
+      }
+      prevNode= currentNode;
+      currentNode = currentNode.next;
+      count++;
+    }
+
+    //if the while loop finishes and it still hasn't been inserted, it should be at the end
+    this.insertLast(item);
   }
 
   //possibly incorrect...
@@ -133,8 +154,8 @@ class LinkedList {
 
     while(currentNode!==null){
       if(currentNode.data===item){
-        prevNode.next = currentNode.next
-        console.log('item removed')
+        prevNode.next = currentNode.next;
+        console.log('item removed');
         return;
       }
       else{
@@ -142,7 +163,7 @@ class LinkedList {
         currentNode = currentNode.next;
       }
     }
-    console.log('item not found')
+    console.log('item not found');
     return;
   }
 }
@@ -150,16 +171,17 @@ class LinkedList {
 function main() {
   const SLL = new LinkedList();
 
-  SLL.insertFirst('Apollo')
-  SLL.insertLast('Boomer')
-  SLL.insertLast('Helo')
-  SLL.insertLast('Husker')
+  SLL.insertFirst('Apollo');
+  SLL.insertLast('Boomer');
+  SLL.insertLast('Helo');
+  SLL.insertLast('Husker');
   // SLL.insertLast('Starbuck')
   // SLL.insertLast('Tauhida')
   // SLL.remove('squirrel')
-  SLL.insertBefore('Athena', 'Husker')
-  SLL.insertAfter('Hotdog', 'Helo')
-  console.log(SLL)
+  SLL.insertBefore('Athena', 'Husker');
+  SLL.insertAfter('Hotdog', 'Helo');
+  SLL.insertAt('Nikkie', 7);
+  console.log(SLL);
 }
 
 main();
